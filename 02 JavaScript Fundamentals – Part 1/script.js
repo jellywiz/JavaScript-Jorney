90f541f70b3b9d7229d8e9849c267d255d96c29e
jellywiz <muzaffer.dler@gmail.com>
2022-10-30 12:58:36 +0300

    Values and Variables

--- 02 JavaScript Fundamentals – Part 1/script.js
+++ 02 JavaScript Fundamentals – Part 1/script.js
@@ -1,5 +1,30 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// if (js === "amazing") alert("JavaScript is FUN!");
 
-40 + 8 + 23 - 10;
+// 40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log("Muzaffer");
+console.log(23);
+
+// Variable name conventions
+let firstName = "Muzaffer";
+let first = "MJ";
+let lastName = "Dler";
+
+console.log(firstName);
+console.log(first);
+console.log(lastName);
+
+let jonas_matilda = "JM";
+let $function = 27;
+
+const PI = 3.1415;
+
+// accepted
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// not accepted
+let job1 = "Programmer";
+let job2 = "Teacher";
