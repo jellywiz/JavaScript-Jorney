98d913c0037b374cb4b562ec8ab31ec13226feee
jellywiz <muzaffer.dler@gmail.com>
2022-10-31 13:07:05 +0300

    Functions

--- 03 JavaScript Fundamentals – Part 2/script.js
+++ 03 JavaScript Fundamentals – Part 2/script.js
@@ -1,3 +1,4 @@
+/*
 // Activating Strict Mode
 'use strict';
 
@@ -6,3 +7,37 @@ const passTest = true;
 
 if (passTest) hasDriverLicense = true;
 if (hasDriversLicense) console.log('I can drive');
+*/
+
+//////////////////////////////////////////////////
+// Functions
+'use strict'
+
+function logger() {
+    console.log('My name is Muzaffer');
+}
+
+// calling, running and invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const Juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return Juice;
+}
+
+//console.log(fruitProcessor(5, 0));
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const nuum = '23'
+console.log(nuum);
+console.log(typeof nuum);
+const num = Number(nuum);
+console.log(num);
+console.log(typeof num);
