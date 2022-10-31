543b673538ceaeb6c901e294e7276e167a69c665
jellywiz <muzaffer.dler@gmail.com>
2022-10-31 11:47:00 +0300

    Activating Strict Mode

---
+++ 03 JavaScript Fundamentals – Part 2/script.js
@@ -0,0 +1,8 @@
+// Activating Strict Mode
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log('I can drive');
