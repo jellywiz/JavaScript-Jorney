7c8a82e5e9e9d444b32e40013b2cb2382ad4223d
jellywiz <muzaffer.dler@gmail.com>
2022-11-20 10:55:07 +0300

    006 Handling Click Events

--- 07 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
+++ 07 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 document.querySelector('.message');
 console.log(document.querySelector('.message'));
 console.log(document.querySelector('.message').textContent);
@@ -12,3 +12,23 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+const checkBtn = document.querySelector('.check');
+
+checkBtn.addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  if (
+    document.querySelector('.guess').value > 20 ||
+    document.querySelector('.guess').value < 0
+  ) {
+    document.querySelector('.guess').style.border = '4px solid red';
+    document.querySelector('.message').textContent =
+      'The number should be between 1 and 20';
+  } else if (!guess) {
+    document.querySelector('.guess').style.border = '4px solid red';
+    document.querySelector('.message').textContent = 'No Number';
+  } else {
+    document.querySelector('.guess').style.border = '4px solid #eee';
+    console.log(typeof guess);
+  }
+});
