225885e0b030113016373122bc279137af61ea0b
jellywiz <muzaffer.dler@gmail.com>
2022-11-20 15:51:05 +0300

    Refactoring Our Code_ The DRY Principle

--- 07 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
+++ 07 JavaScript in the Browser_ DOM and Events Fundamentals/script.js
@@ -14,10 +14,14 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 const checkBtn = document.querySelector('.check');
-let randomNum = Math.trunc(Math.random() * 20) + 1;
 const number = document.querySelector('.number');
+let randomNum = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+
+const displayMessage = message => {
+  document.querySelector('.message').textContent = message;
+};
 
 checkBtn.addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,11 +30,10 @@ checkBtn.addEventListener('click', () => {
     document.querySelector('.guess').value < 0
   ) {
     document.querySelector('.guess').style.border = '4px solid red';
-    document.querySelector('.message').textContent =
-      'The number should be between 1 and 20';
+    displayMessage('The number should be between 1 and 20');
   } else if (!guess) {
     document.querySelector('.guess').style.border = '4px solid red';
-    document.querySelector('.message').textContent = 'No Number';
+    displayMessage('No Number');
   } else {
     document.querySelector('.guess').style.border = '4px solid #eee';
     if (guess === randomNum && score > 0) {
@@ -42,26 +45,37 @@ checkBtn.addEventListener('click', () => {
         highscore = score;
         document.querySelector('.highscore').textContent = score;
       }
-    } else if (guess > randomNum) {
+    } else if (guess !== randomNum) {
       if (score > 1) {
-        document.querySelector('.message').textContent = 'Too High';
+        displayMessage(guess > randomNum ? 'Too High' : 'Too Low');
         score -= 1;
       } else {
-        document.querySelector('.message').textContent =
-          'Game Over! Please try again';
+        displayMessage('Game Over! Please try again');
+        document.querySelector('body').style.backgroundColor = 'Red';
         score = 0;
       }
-    } else {
-      if (score > 1) {
-        document.querySelector('.message').textContent = 'Too Low';
-        score -= 1;
-      } else {
-        document.querySelector('.message').textContent =
-          'Game Over! Please try again';
-        score = 0;
-      }
+      document.querySelector('.score').textContent = score;
     }
-    document.querySelector('.score').textContent = score;
+    // else if (guess > randomNum) {
+    //   if (score > 1) {
+    //     document.querySelector('.message').textContent = 'Too High';
+    //     score -= 1;
+    //   } else {
+    //     document.querySelector('.message').textContent =
+    //       'Game Over! Please try again';
+    //     score = 0;
+    //   }
+    // } else {
+    //   if (score > 1) {
+    //     document.querySelector('.message').textContent = 'Too Low';
+    //     score -= 1;
+    //   } else {
+    //     document.querySelector('.message').textContent =
+    //       'Game Over! Please try again';
+    //     score = 0;
+    //   }
+    // }
+    // document.querySelector('.score').textContent = score;
   }
 });
 
@@ -70,7 +84,7 @@ const againBtn = document.querySelector('.again');
 againBtn.addEventListener('click', () => {
   score = 20;
   randomNum = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.guess').style.border = '4px solid #eee';
   document.querySelector('.guess').value = null;
   document.querySelector('.score').textContent = score;
@@ -84,51 +98,58 @@ const guessInput = document.querySelector('.guess');
 guessInput.addEventListener('keypress', event => {
   const guess = Number(document.querySelector('.guess').value);
   if (event.key === 'Enter') {
+    const guess = Number(document.querySelector('.guess').value);
     if (
       document.querySelector('.guess').value > 20 ||
       document.querySelector('.guess').value < 0
     ) {
       document.querySelector('.guess').style.border = '4px solid red';
-      document.querySelector('.message').textContent =
-        'The number should be between 1 and 20';
+      displayMessage('The number should be between 1 and 20');
     } else if (!guess) {
       document.querySelector('.guess').style.border = '4px solid red';
-      document.querySelector('.message').textContent = 'No Number';
+      displayMessage('No Number');
     } else {
       document.querySelector('.guess').style.border = '4px solid #eee';
       if (guess === randomNum && score > 0) {
         document.querySelector('.number').style.width = '30rem';
         document.querySelector('body').style.backgroundColor = 'green';
-        document.querySelector('.message').textContent = 'Well Done Correct';
+        displayMessage('Well Done Correct');
         number.textContent = randomNum;
         if (highscore < score) {
           highscore = score;
           document.querySelector('.highscore').textContent = score;
         }
-      } else if (guess > randomNum) {
+      } else if (guess !== randomNum) {
         if (score > 1) {
-          document.querySelector('.message').textContent = 'Too High';
+          displayMessage(guess > randomNum ? 'Too High' : 'Too Low');
           score -= 1;
         } else {
-          document.querySelector('.message').textContent =
-            'Game Over! Please try again';
+          displayMessage('Game Over! Please try again');
+          document.querySelector('body').style.backgroundColor = 'Red';
           score = 0;
         }
-      } else {
-        if (score > 1) {
-          document.querySelector('.message').textContent = 'Too Low';
-          score -= 1;
-        } else {
-          document.querySelector('.message').textContent =
-            'Game Over! Please try again';
-          score = 0;
-        }
+        document.querySelector('.score').textContent = score;
       }
+      // else if (guess > randomNum) {
+      //   if (score > 1) {
+      //     document.querySelector('.message').textContent = 'Too High';
+      //     score -= 1;
+      //   } else {
+      //     document.querySelector('.message').textContent =
+      //       'Game Over! Please try again';
+      //     score = 0;
+      //   }
+      // } else {
+      //   if (score > 1) {
+      //     document.querySelector('.message').textContent = 'Too Low';
+      //     score -= 1;
+      //   } else {
+      //     document.querySelector('.message').textContent =
+      //       'Game Over! Please try again';
+      //     score = 0;
+      //   }
+      // }
+      // document.querySelector('.score').textContent = score;
     }
-    if (score <= 0) {
-      document.querySelector('.message').textContent =
-        'Game Over! Please try again';
-    }
-    document.querySelector('.score').textContent = score;
   }
 });
