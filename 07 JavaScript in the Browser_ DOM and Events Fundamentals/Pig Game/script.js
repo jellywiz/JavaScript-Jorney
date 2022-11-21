d6b53beceba75bebe8474f67010c156ca59019a5
jellywiz <muzaffer.dler@gmail.com>
2022-11-21 11:42:17 +0300

    switching player

--- 07 JavaScript in the Browser_ DOM and Events Fundamentals/Pig Game/script.js
+++ 07 JavaScript in the Browser_ DOM and Events Fundamentals/Pig Game/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -9,7 +11,9 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -26,8 +30,15 @@ btnRoll.addEventListener('click', () => {
   if (dice !== 1) {
     // Add dice to current Score
     currentScore += dice;
-    current0El.textContent = currentScore; // change later
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0El.textContent = currentScore; // change later
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active'); // add if its not there/ remove if its there
+    player1El.classList.toggle('player--active');
   }
 });
