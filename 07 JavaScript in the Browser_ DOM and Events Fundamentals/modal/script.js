3f76a958fe712926d4ff323e5f52f9c91396b578
jellywiz <muzaffer.dler@gmail.com>
2022-11-21 10:00:42 +0300

    Working With Classes

--- 07 JavaScript in the Browser_ DOM and Events Fundamentals/modal/script.js
+++ 07 JavaScript in the Browser_ DOM and Events Fundamentals/modal/script.js
@@ -4,7 +4,31 @@ const btnsOpenModel = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const bntCloseModel = document.querySelector('.close-modal');
+const closeModel = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 for (let i = 0; i < btnsOpenModel.length; i++) {
-  console.log(btnsOpenModel[i].textContent);
+  btnsOpenModel[i].addEventListener('click', openModal);
+  //   btnsOpenModel[i].addEventListener('click', () => {
+  //     modal.classList.remove('hidden');
+  //     overlay.classList.remove('hidden');
+  //   });
 }
+
+bntCloseModel.addEventListener('click', closeModel);
+overlay.addEventListener('click', closeModel);
+// bntCloseModel.addEventListener('click', () => {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
+
+// overlay.addEventListener('click', () => {
+//   modal.classList.add('hidden');
+//   overlay.classList.add('hidden');
+// });
